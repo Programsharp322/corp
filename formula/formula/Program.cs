159b340fa3321581cfad08ed2ed6c097fc397672
prohramsharp <nevionn@yandex.ru>
2022-07-04 18:16:29 +0300

    test

---
+++ formula/formula/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("На сколько процентов первое число больше второго\n---------------------------------------------");
+            Console.WriteLine("Введите первое число");
+           double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine("на " + Math.Round((num1/num2*100-100),1) + "%% ");
+
+           // double result = (num1 / num2 * 100 - 100);
+           // Console.WriteLine(Math.Round(result,1));
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
